16add4bc8a3c236d6b5da6c756a906d785f947cd
Yashar <ya.atajan@gmail.com>
2018-11-03 22:40:57 -0400

    web update

--- Data/DataGenerator.py
+++ Data/DataGenerator.py
@@ -6,14 +6,14 @@ change = 0.013
 text_file = open("Output.txt", "w")
 
 for x in range(0, 10000):
-    weight = random.uniform(0, 1.5)
+    weight = random.uniform(0.999, 1.001)
 
     if 0.95 <= weight <= 1.05:
         current += change
         current *= weight
-        text_file.write(",%s" % current)
+        text_file.write("\n%s" % current)
     else:
         current += change
-        text_file.write(",%s" % current)
+        text_file.write("\n%s" % current)
 
 text_file.close()
